a56e7a4a9d1c634b01d15c1795e5d02e695caaf0
未央 <hiveforwork@163.com>
2019-12-18 23:02:48 +0800

    完善home页，分离菜单组件

---
+++ src/components/MyMenu.vue
@@ -0,0 +1,75 @@
+<template>
+    <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
+        <MenuItem name="1-1">
+            <Icon type="ios-navigate"></Icon>
+            <span>Option 1</span>
+        </MenuItem>
+        <MenuItem name="1-2">
+            <Icon type="ios-search"></Icon>
+            <span>Option 2</span>
+        </MenuItem>
+        <MenuItem name="1-3">
+            <Icon type="ios-settings"></Icon>
+            <span>Option 3</span>
+        </MenuItem>
+    </Menu>
+</template>
+
+<script>
+    export default {
+        name: "MyMenu",
+        props: {
+            isCollapsed: {
+                type: Boolean,
+                default: false
+            }
+        },
+        computed: {
+            menuitemClasses() {
+                return [
+                    'menu-item',
+                    this.isCollapsed ? 'collapsed-menu' : ''
+                ]
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    .menu-icon {
+        transition: all .3s;
+    }
+
+    .rotate-icon {
+        transform: rotate(-90deg);
+    }
+
+    .menu-item span {
+        display: inline-block;
+        overflow: hidden;
+        width: 69px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        vertical-align: bottom;
+        transition: width .2s ease .2s;
+    }
+
+    .menu-item i {
+        transform: translateX(0px);
+        transition: font-size .2s ease, transform .2s ease;
+        vertical-align: middle;
+        font-size: 16px;
+    }
+
+    .collapsed-menu span {
+        width: 0px;
+        transition: width .2s ease;
+    }
+
+    .collapsed-menu i {
+        transform: translateX(5px);
+        transition: font-size .2s ease .2s, transform .2s ease .2s;
+        vertical-align: middle;
+        font-size: 22px;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,115 +1,86 @@
 <template>
-  <div class="home">
-    <Layout>
-      <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
-        <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
-          <MenuItem name="1-1">
-            <Icon type="ios-navigate"></Icon>
-            <span>Option 1</span>
-          </MenuItem>
-          <MenuItem name="1-2">
-            <Icon type="ios-search"></Icon>
-            <span>Option 2</span>
-          </MenuItem>
-          <MenuItem name="1-3">
-            <Icon type="ios-settings"></Icon>
-            <span>Option 3</span>
-          </MenuItem>
-        </Menu>
-      </Sider>
-      <Layout>
-        <Header :style="{padding: 0}" class="layout-header-bar">
-          <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '0 20px'}" type="md-menu" size="24"></Icon>
-        </Header>
-        <Content :style="{margin: '20px', background: '#fff', minHeight: '260px'}">
-          Content
-        </Content>
-      </Layout>
-    </Layout>
-  </div>
+    <div class="home">
+        <Layout>
+            <Sider class="sider" :style="{ minHeight: minHeight + 'px' }" ref="side1" hide-trigger collapsible
+                   :collapsed-width="78" v-model="isCollapsed">
+                <div class="logo">
+                    <img v-show="!isCollapsed" src="../assets/logo.png" width="122px" height="27px">
+                </div>
+                <MyMenu :isCollapsed="isCollapsed"></MyMenu>
+            </Sider>
+            <Layout>
+                <Header :style="{padding: 0}" class="layout-header-bar">
+                    <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '0 20px'}" type="md-menu"
+                          size="24"></Icon>
+                </Header>
+                <Content :style="{margin: '20px', background: '#fff', minHeight: '260px'}">
+                    Content
+                </Content>
+            </Layout>
+        </Layout>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-export default {
-  name: 'home',
-  components: {
-  },
-  data () {
-    return {
-      isCollapsed: false
+    // @ is an alias to /src
+    import MyMenu from '../components/MyMenu.vue';
+
+    export default {
+        name: 'home',
+        components: {
+            MyMenu,
+        },
+        data() {
+            return {
+                isCollapsed: false,
+                minHeight: null,
+            }
+        },
+        mounted() {
+            this.minHeight = document.documentElement.clientHeight;
+        },
+        computed: {
+            rotateIcon() {
+                return [
+                    'menu-icon',
+                    this.isCollapsed ? 'rotate-icon' : ''
+                ];
+            },
+
+        },
+        methods: {
+            collapsedSider() {
+                this.$refs.side1.toggleCollapse();
+            }
+        }
     }
-  },
-  computed: {
-    rotateIcon () {
-      return [
-        'menu-icon',
-        this.isCollapsed ? 'rotate-icon' : ''
-      ];
-    },
-    menuitemClasses () {
-      return [
-        'menu-item',
-        this.isCollapsed ? 'collapsed-menu' : ''
-      ]
-    }
-  },
-  methods: {
-    collapsedSider () {
-      this.$refs.side1.toggleCollapse();
-    }
-  }
-}
 </script>
 
 <style scoped>
-  .home {
-    border: 1px solid #d7dde4;
-    background: #f5f7f9;
-    position: relative;
-    border-radius: 4px;
-    overflow: hidden;
-  }
-  .layout-header-bar{
-    background: #fff;
-    box-shadow: 0 1px 1px rgba(0,0,0,.1);
-  }
-  .layout-logo-left{
-    width: 90%;
-    height: 30px;
-    background: #5b6270;
-    border-radius: 3px;
-    margin: 15px auto;
-  }
-  .menu-icon{
-    transition: all .3s;
-  }
-  .rotate-icon{
-    transform: rotate(-90deg);
-  }
-  .menu-item span{
-    display: inline-block;
-    overflow: hidden;
-    width: 69px;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    vertical-align: bottom;
-    transition: width .2s ease .2s;
-  }
-  .menu-item i{
-    transform: translateX(0px);
-    transition: font-size .2s ease, transform .2s ease;
-    vertical-align: middle;
-    font-size: 16px;
-  }
-  .collapsed-menu span{
-    width: 0px;
-    transition: width .2s ease;
-  }
-  .collapsed-menu i{
-    transform: translateX(5px);
-    transition: font-size .2s ease .2s, transform .2s ease .2s;
-    vertical-align: middle;
-    font-size: 22px;
-  }
+    .home {
+        width: 100%;
+        height: 100%;
+        background: #f5f7f9;
+        position: relative;
+        overflow: hidden;
+    }
+
+    .layout-header-bar {
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0, 0, 0, .1);
+    }
+
+
+    .sider {
+        background-color: #20222A;
+    }
+
+    .logo {
+        height: 64px;
+        text-align: center;
+        padding-top: 18px;
+        background-color: #20222A;
+        border-bottom: 1px solid gray;
+    }
+
 </style>
